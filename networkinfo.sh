abb229f3c1b3ffffac2390bc38b8cefad34a6b50
seeger <adaniell@oakland.edu>
2020-03-21 16:55:50 -0400

    Started bash script

--- networkinfo.sh
+++ networkinfo.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
-ip a s | cut -d" " -f 2 | cut -d":" -f 1 | grep -v "^$"
-df -h | grep -v loop | grep -v tm
-
+$(./netinfo.py >> sysInfo/file$(date +"%m-%d-%y-%T").log)
+#rsync -aAXv --delete -e ssh sysInfo/ user@centralServer:/path/to/logs.
 
 
