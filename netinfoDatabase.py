a939d9d0e1856c3ea17ae6be0a7c666ef0209fd0
saltrupiao <strupiano@oakland.edu>
2020-04-07 11:06:00 -0400

    ADd salNetinfoDatabase.py - modified python file for database insert statements

--- netinfoDatabase.py
+++ netinfoDatabase.py
@@ -40,61 +40,61 @@ def gatherInfo():
 
    # print("\nNetwork info\n------------")
 
-    net = psutil.net_if_addrs()
-    if len(net.keys()) > 1:
-        global netCard 
-        netCard = net.keys()[1]
-        count = 0
-        for interface in net[netCard]: 
-            if count == 0:
-                global ipv4Address
-                ipv4Address = str(interface[1])
-            elif count == 1:
-                global ipv6Address
-                ipv6Address = str(interface[1])
-            elif count == 2:
-                global macAddress
-                macAddress= str(interface[1])
-            count = count + 1
-    else:
-        print("Only loopback interface available")
+#    net = psutil.net_if_addrs()
+#    if len(net.keys()) > 1:
+#        global netCard
+#        netCard = net.keys()[1]
+#        count = 0
+#        for interface in net[netCard]:
+#            if count == 0:
+#                global ipv4Address
+#                ipv4Address = str(interface[1])
+#            elif count == 1:
+#                global ipv6Address
+#                ipv6Address = str(interface[1])
+#            elif count == 2:
+#                global macAddress
+#                macAddress= str(interface[1])
+#            count = count + 1
+#    else:
+#        print("Only loopback interface available")
 
 def insert():
     with open('passwd.txt', 'r') as my_file:
         password = my_file.read().rstrip()
 
-    connection = pymysql.connect(host="35.237.92.151", 
-                                 user="root", 
+    connection = pymysql.connect(host="35.196.30.1",
+                                 user="user",
                                  passwd = password, 
                                  db="sysmonitor",
                                  charset='utf8mb4',
                                  cursoclass=pymysql.cursors.DictCursor)
-    sql = "INSERT INTO 'sysmonitor' () VALUES (%s, %s)" 
-    cursor.execute(sql, (memTot, memRemain, memPercent, dskAvail, dskUsed,))
+    #sql = "INSERT INTO `client` (`cli_hostname`,`cli_mem_total`, `cli_mem_remaining`, `cli_mem_pctg`, `cli_disk_avail`, `cli_disk_used`) VALUES (%s, %s)"
+    #cursor.execute(sql, (hostName, memTot, memRemain, memPercent, dskAvail, dskUsed,))
     try:
         with connection.cursor() as cursor:
             #Create a new record
-            sql = "INSERT INTO 'sysmonitor' () VALUES (%s, %s)"
-            cursor.execute(sql, (memTot, memRemain, memPercent, dskAvail, dskUsed,))
+            sql = "INSERT INTO `client` (`cli_hostname`, `cli_mem_total`, `cli_mem_remaining`, `cli_mem_pctg`, `cli_disk_avail`, `cli_disk_used`) VALUES (%s, %s)"
+            cursor.execute(sql, (hostName, memTot, memRemain, memPercent, dskAvail, dskUsed,))
         cursor.commit()
     finally:
         connection.close()
 
-def printInfo():
-    global info
-    info = "Hostname: " + hostName +"\nMemory\n------\nMemory Total: " + memTot  + "Gb\nMemory Remaining: " + memRemain + "Gb\nMemory Percentage: " + memPercent + "%\n" + "Disk Usage\n----------\nDisk Total: " + dskTot + "Gb\nDisk Available: " + dskAvail + "Gb\nDisk Used: " + dskUsed + "Gb\nPercent Used: " + dskPercent + "%\n" + "CPU\n---\nCPU Percentage: " + cpuCore + "%\n" +"Networking Info\n---------------\nInterface: " + netCard + "\nIPv4 address: " + ipv4Address + "\nIPv6 Address: " + ipv6Address + "\nMAC Address: " + macAddress
-    print(info)
+#def printInfo():
+#    global info
+#    info = "Hostname: " + hostName +"\nMemory\n------\nMemory Total: " + memTot  + "Gb\nMemory Remaining: " + memRemain + "Gb\nMemory Percentage: " + memPercent + "%\n" + "Disk Usage\n----------\nDisk Total: " + dskTot + "Gb\nDisk Available: " + dskAvail + "Gb\nDisk Used: " + dskUsed + "Gb\nPercent Used: " + dskPercent + "%\n" + "CPU\n---\nCPU Percentage: " + cpuCore + "%\n" +"Networking Info\n---------------\nInterface: " + netCard + "\nIPv4 address: " + ipv4Address + "\nIPv6 Address: " + ipv6Address + "\nMAC Address: " + macAddress
+#    print(info)
 
-def createLog():
-    timestamp = time.strftime("%Y-%m-%dT%H-%M")
-    destFile = hostName + timestamp + ".log"
-    my_file = open(destFile, 'ab')
-    my_file.writelines(info)
-    my_file.close()
+#def createLog():
+#    timestamp = time.strftime("%Y-%m-%dT%H-%M")
+#    destFile = hostName + timestamp + ".log"
+#    my_file = open(destFile, 'ab')
+#    my_file.writelines(info)
+#    my_file.close()
 
 def main():
     gatherInfo()
-    printInfo()
-    createLog()
+#    printInfo()
+#    createLog()
 
 main()
