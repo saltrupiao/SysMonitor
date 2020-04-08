de18d4ef8b0888cfc756f8907e3277b84ada3229
JoshBrudnak <jobrud314@gmail.com>
2020-04-07 21:38:47 -0400

    Fixen stuff

--- assets/js/diskBarChart.js
+++ assets/js/diskBarChart.js
@@ -2,116 +2,116 @@ var ctx = document.getElementById('diskBarChart');
 
 Chart.defaults.global.defaultFontColor = '#fff';
 
-var dts
-
-function client_data() {
+async client_data() {
   getClientPerfData()
       .then(data => {
         console.log(data)
         let data = data.system_performance_data
 
-  let disk_free = []
-  data.forEach(c => disk_free.append(c.disk_free))
+        let disk_free = []
+        data.forEach(c => disk_free.append(c.disk_free))
 
-  let disk_usage = []
-  data.forEach(c => disk_usage.append(c.disk_used))
+        let disk_usage = []
+        data.forEach(c => disk_usage.append(c.disk_used))
 
-  let disk_total = []
-  data.forEach(c => disk_used.append(c.disk_total))
+        let disk_total = []
+        data.forEach(c => disk_used.append(c.disk_total))
 
-  dts = [
-    {
-        label : "Disk Free (MB)",
-        data : disk_free,
-        backgroundColor : [
-            'rgba(255, 255, 0, 0.5)',  /* Yellow */
-            'rgba(255, 255, 0, 0.5)',
-            'rgba(255, 255, 0, 0.5)',
-            'rgba(255, 255, 0, 0.5)',
-            'rgba(255, 255, 0, 0.5)',
-            'rgba(255, 255, 0, 0.5)'
-        ],
-        borderColor : [
-            'rgba(255, 255, 0, 1)',
-            'rgba(255, 255, 0, 1)',
-            'rgba(255, 255, 0, 11)',
-            'rgba(255, 255, 0, 1)',
-            'rgba(255, 255, 0, 1)',
-            'rgba(255, 255, 0, 1)'
-        ],
-        borderWidth : 1
-    },
-    {
-        label : "Disk Usage (%)",
-        data : disk_usage,
-        backgroundColor : [
-            'rgba(204, 204, 0, 0.5)',  /* Dark-yellow1 */
-            'rgba(204, 204, 0, 0.5)',
-            'rgba(204, 204, 0, 0.5)',
-            'rgba(204, 204, 0, 0.5)',
-            'rgba(204, 204, 0, 0.5)',
-            'rgba(204, 204, 0, 0.5)'
-        ],
-        borderColor : [
-            'rgba(204, 204, 0, 1)',
-            'rgba(204, 204, 0, 1)',
-            'rgba(204, 204, 0, 1)',
-            'rgba(204, 204, 0, 1)',
-            'rgba(204, 204, 0, 1)',
-            'rgba(204, 204, 0, 1)'
-        ],
-        borderWidth : 1
-    },
-    {
-        label : "Disk Usage (MB)",
-        data : disk_total, // TODO: FIX THIS :)
-        backgroundColor : [
-            'rgba(153, 153, 0, 0.5)',  /* Dark-yellow2 */
-            'rgba(153, 153, 0, 0.5)',
-            'rgba(153, 153, 0, 0.5)',
-            'rgba(153, 153, 0, 0.5)',
-            'rgba(153, 153, 0, 0.5)',
-            'rgba(153, 153, 0, 0.5)'
-        ],
-        borderColor : [
-            'rgba(153, 153, 0, 1)',
-            'rgba(153, 153, 0, 1)',
-            'rgba(153, 153, 0, 1)',
-            'rgba(153, 153, 0, 1)',
-            'rgba(153, 153, 0, 1)',
-            'rgba(153, 153, 0, 1)'
-        ],
-        borderWidth : 1
-    },
-    {
-        label : "Disk Used (MB)",
-        data : disk_used,
-        backgroundColor : [
-            'rgba(102, 102, 0, 0.5)',  /* Dark-yellow3 */
-            'rgba(102, 102, 0, 0.5)',
-            'rgba(102, 102, 0, 0.5)',
-            'rgba(102, 102, 0, 0.5)',
-            'rgba(102, 102, 0, 0.5)',
-            'rgba(102, 102, 0, 0.5)'
-        ],
-        borderColor : [
-            'rgba(102, 102, 0, 1)',
-            'rgba(102, 102, 0, 1)',
-            'rgba(102, 102, 0, 1)',
-            'rgba(102, 102, 0, 1)',
-            'rgba(102, 102, 0, 1)',
-            'rgba(102, 102, 0, 1)'
-        ],
-        borderWidth : 1
-    }
-  ];
+        let dts = [
+          {
+              label : "Disk Free (MB)",
+              data : disk_free,
+              backgroundColor : [
+                  'rgba(255, 255, 0, 0.5)',  /* Yellow */
+                  'rgba(255, 255, 0, 0.5)',
+                  'rgba(255, 255, 0, 0.5)',
+                  'rgba(255, 255, 0, 0.5)',
+                  'rgba(255, 255, 0, 0.5)',
+                  'rgba(255, 255, 0, 0.5)'
+              ],
+              borderColor : [
+                  'rgba(255, 255, 0, 1)',
+                  'rgba(255, 255, 0, 1)',
+                  'rgba(255, 255, 0, 11)',
+                  'rgba(255, 255, 0, 1)',
+                  'rgba(255, 255, 0, 1)',
+                  'rgba(255, 255, 0, 1)'
+              ],
+              borderWidth : 1
+          },
+          {
+              label : "Disk Usage (%)",
+              data : disk_usage,
+              backgroundColor : [
+                  'rgba(204, 204, 0, 0.5)',  /* Dark-yellow1 */
+                  'rgba(204, 204, 0, 0.5)',
+                  'rgba(204, 204, 0, 0.5)',
+                  'rgba(204, 204, 0, 0.5)',
+                  'rgba(204, 204, 0, 0.5)',
+                  'rgba(204, 204, 0, 0.5)'
+              ],
+              borderColor : [
+                  'rgba(204, 204, 0, 1)',
+                  'rgba(204, 204, 0, 1)',
+                  'rgba(204, 204, 0, 1)',
+                  'rgba(204, 204, 0, 1)',
+                  'rgba(204, 204, 0, 1)',
+                  'rgba(204, 204, 0, 1)'
+              ],
+              borderWidth : 1
+          },
+          {
+              label : "Disk Usage (MB)",
+              data : disk_total, // TODO: FIX THIS :)
+              backgroundColor : [
+                  'rgba(153, 153, 0, 0.5)',  /* Dark-yellow2 */
+                  'rgba(153, 153, 0, 0.5)',
+                  'rgba(153, 153, 0, 0.5)',
+                  'rgba(153, 153, 0, 0.5)',
+                  'rgba(153, 153, 0, 0.5)',
+                  'rgba(153, 153, 0, 0.5)'
+              ],
+              borderColor : [
+                  'rgba(153, 153, 0, 1)',
+                  'rgba(153, 153, 0, 1)',
+                  'rgba(153, 153, 0, 1)',
+                  'rgba(153, 153, 0, 1)',
+                  'rgba(153, 153, 0, 1)',
+                  'rgba(153, 153, 0, 1)'
+              ],
+              borderWidth : 1
+          },
+          {
+              label : "Disk Used (MB)",
+              data : disk_used,
+              backgroundColor : [
+                  'rgba(102, 102, 0, 0.5)',  /* Dark-yellow3 */
+                  'rgba(102, 102, 0, 0.5)',
+                  'rgba(102, 102, 0, 0.5)',
+                  'rgba(102, 102, 0, 0.5)',
+                  'rgba(102, 102, 0, 0.5)',
+                  'rgba(102, 102, 0, 0.5)'
+              ],
+              borderColor : [
+                  'rgba(102, 102, 0, 1)',
+                  'rgba(102, 102, 0, 1)',
+                  'rgba(102, 102, 0, 1)',
+                  'rgba(102, 102, 0, 1)',
+                  'rgba(102, 102, 0, 1)',
+                  'rgba(102, 102, 0, 1)'
+              ],
+              borderWidth : 1
+          }
+        ]
+
+        return dts
       })
       .catch(error => {
         console.error(error)
       })
 }
 
-const getClientPerfData = async () => {
+const getClientPerfData = () => {
   const response = await fetch("http://35.196.30.1:8080/perfdata")
   const newData = await response.json()
 
@@ -121,8 +121,8 @@ const getClientPerfData = async () => {
 var data = {
     labels: ["Client 1", "Client 2", "Client 3", "Client 4", "Client 5"],
     fontColor: '#fff',
-    datasets: client_data()
-};
+    datasets: await client_data()
+}
 
 var lineGraph = new Chart(ctx, {
     type: 'bar',
@@ -137,7 +137,7 @@ var lineGraph = new Chart(ctx, {
             }]
         }
     }
-});
+})
 
 function addData(chart, label, data) {
     chart.data.labels.push(label);
