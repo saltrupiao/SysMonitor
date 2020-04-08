1ca2ddd5a82fc7e19879f58caa2bebe5b37d61aa
saltrupiao <strupiano@oakland.edu>
2020-04-08 13:19:13 -0400

    Added sendJSON.py - get rid of underscore

--- controller/sendJSON.py
+++ controller/sendJSON.py
@@ -4,11 +4,7 @@ def gatherInfo():
     output = subprocess.check_output(['curl', '34.71.205.185/perfdata'])
     data = json.loads(output)
 
-    f = open("../assets/js/perf_data.json", "a")
+    f = open("../assets/js/perfdata.json", "a")
     f.write(data)
     f.close()
 gatherInfo()
-
-
-
-
