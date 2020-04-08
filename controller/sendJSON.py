5821d55d9049d05dbdab0a9bded1d913753a3c8a
saltrupiao <strupiano@oakland.edu>
2020-04-08 13:33:20 -0400

    Removed JSON.loads decode function

--- controller/sendJSON.py
+++ controller/sendJSON.py
@@ -2,9 +2,8 @@ import subprocess
 import json
 def gatherInfo():
     output = subprocess.check_output(['curl', '34.71.205.185/perfdata'])
-    data = json.loads(output)
 
     f = open("../assets/js/perfdata.json", "a")
-    f.write(data)
+    f.write(output)
     f.close()
 gatherInfo()
