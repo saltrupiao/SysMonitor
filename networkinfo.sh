8976731887626392fac0f6437bccfceff13e341d
seeger <adaniell@oakland.edu>
2020-04-10 22:02:09 -0400

    Moved files to merge into master

--- networkinfo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-$(./netinfo.py >> sysInfo/file$(date +"%m-%d-%y-%T").log)
-#rsync -aAXv --delete -e ssh sysInfo/ user@centralServer:/path/to/logs.
-rsync -aAXv -e ssh /home/frozenpizzas4567/sysInfo/ frozenpizzas4567@34.237.92.151:/usr/local/share/sysInfo/
-
-
